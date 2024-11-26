35c558c7394d10a52e041fd69c0d9fdf57c903ec
Kauesilva123 <dasilvaruizk@gmail.com>
2024-11-26 14:40:32 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Língua Inglesa',
+    'O que a palavra legend significa em português?',
+    'Lenda'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
